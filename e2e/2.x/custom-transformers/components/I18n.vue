bdd074d7575ed6c9a589d6a98b059e742ed64814
kazuya kawaguchi <kawakazu80@gmail.com>
2021-07-10 13:35:18 +0900

    feat: support custom blocks for Vue 3.x

---
+++ e2e/2.x/custom-transformers/components/I18n.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>i18n custom block</p>
+</template>
+
+<script>
+export default {
+  name: 'I18n'
+}
+</script>
+
+<i18n>
+{
+  "en": {
+    "hello": "Hello!"
+  },
+  "ja": {
+    "hello": "こんにちは！"
+  }
+}
+</i18n>
